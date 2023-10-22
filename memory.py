2f43e47c28819172916db15d5ded639c1091e23c
laslo23 <vladyslav_hryshchenko@proton.me>
2023-10-22 16:27:52 +0300

    Add timer

--- memory.py
+++ memory.py
@@ -4,14 +4,14 @@ import tkinter as tk
 class Memory:
     def __init__(self):
         self.window = tk.Tk()
-        # self.window.geometry("600x700")
-
+        self.window.geometry("600x700")
         self.window.title("Memory Training")
         
         #Field for entering the size of the matrix
         self.size_entry = tk.Entry(self.window)
         self.size_entry.pack()
 
+        #Generate button
         self.generate_button = tk.Button(self.window, text="Generate", command=lambda: self.generate_matrix(int(self.size_entry.get())))
         self.generate_button.pack()
 
@@ -19,12 +19,16 @@ class Memory:
         self.matrix_label = tk.Label(self.window)
         self.matrix_label.pack()
 
+
+        # Label for the timer
+        self.timer_label = tk.Label(self.window, text="05:00", font=("Arial", 20))
+        self.timer_label.pack(side="top")
+
     def generate_matrix(self, size):
-        
-        # Generate the matrix
+        #Generate the matrix
         matrix = [[random.randint(1, 23) for _ in range(size)] for _ in range(size)]
 
-        # Create matrix components
+        #Create matrix components
         self.labels = []
         for row in matrix:
             for number in row:
@@ -33,13 +37,31 @@ class Memory:
                 label.config(borderwidth=2, highlightthickness=2, highlightcolor="black")
                 self.labels.append(label)
 
-        # Place the components of the matrix in the cells
+        #Place the components of the matrix in the cells
         for row in range(size):
             for column in range(size):
                 self.labels[row * size + column].grid(row=row, column=column)
 
-        # Set the size of the window
+        #Set the size of the window
         self.window.geometry(f"{size * 150}x{size * 180}")
+
+        #Start the timer
+        self.timer_count = 300
+        self.update_timer()
+
+    def update_timer(self):
+        #Decrement the timer count
+        self.timer_count -= 1
+
+        #Update the timer label
+        self.timer_label.config(text=f"{self.timer_count // 60:02d}:{self.timer_count % 60:02d}")
+
+        #If the timer has expired, end the game
+        if self.timer_count == 0:
+            self.end_game()
+
+        #Schedule the next timer update
+        self.window.after(1000, self.update_timer)
 
     def mainloop(self):
         self.window.mainloop()
